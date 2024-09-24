92f59dcc23bf4d8d467f9f60a1820b2d0c603896
konickSGS <konick.gevurah@gmail.com>
2024-09-24 12:01:39 +0300

    Добавление entities классов

---
+++ src/main/java/gs/konick/model/Order.java
@@ -0,0 +1,12 @@
+package gs.konick.model;
+
+import java.util.Date;
+
+public class Order {
+    private long id;
+    private long userId;
+    //private Status status;
+    private int total;
+    private long managerId;
+    private Date createDate;
+}
---
+++ src/main/java/gs/konick/model/User.java
@@ -0,0 +1,61 @@
+package gs.konick.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class User implements Serializable {
+    private long id;
+    private String login;
+    private long roleId;
+    private String email;
+    private String address;
+    private Date createDate;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+}
