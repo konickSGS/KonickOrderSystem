0dde8b71b508a38c3ae9ed95968468d753bbbe03
konickSGS <konick.gevurah@gmail.com>
2024-09-26 02:00:04 +0300

    Добавлен util класс для хеширования пароля

---
+++ src/main/java/gs/konick/utils/HashPassword.java
@@ -0,0 +1,36 @@
+package gs.konick.utils;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Objects;
+
+/**
+ * Класс для хеширования пароля
+ */
+public class HashPassword {
+    private static final String ALGORITHM = "SHA-256";
+    private static final int SIZE = 128;
+
+    private static final MessageDigest md;
+
+    static {
+        try {
+            md = MessageDigest.getInstance(ALGORITHM);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String hash(char[] password) {
+        // Преобразуем пароль в байтовый массив и вычисляем хэш-значение
+        byte[] byteHash = md.digest(new String(password).getBytes(StandardCharsets.UTF_8));
+        return Base64.getEncoder().encodeToString(byteHash);
+    }
+
+    public static boolean verify(char[] password, String token) {
+        return Objects.equals(hash(password), token);
+    }
+
+}
