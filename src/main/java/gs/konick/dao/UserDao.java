ea2a0124222519b322b95b457c4d854554f12ae4
konickSGS <konick.gevurah@gmail.com>
2024-09-26 01:59:38 +0300

    Добавлены реализации getUserById, changePassword, changeRole

--- src/main/java/gs/konick/dao/UserDao.java
+++ src/main/java/gs/konick/dao/UserDao.java
@@ -10,5 +10,18 @@ public interface UserDao extends Dao<User, Long> {
 
     List<User> getAllUsers();
 
+    User getUserById(Long id);
+
+    @Override
+    default User find(Long id) {
+        return getUserById(id);
+    }
+
+    User logIn(String login, String password);
+
+    void signUp(String login, String password);
+
+    void changePassword(long userId, String password);
+
     void changeRole(long userId, int roleId);
 }
--- src/main/java/gs/konick/dao/mysql/MySqlUserDao.java
+++ src/main/java/gs/konick/dao/mysql/MySqlUserDao.java
@@ -4,6 +4,7 @@ import gs.konick.db.ConnectionPool;
 import gs.konick.dao.UserDao;
 import gs.konick.model.User;
 import gs.konick.sql.UserSqlQuery;
+import gs.konick.utils.HashPassword;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -29,7 +30,7 @@ public class MySqlUserDao implements UserDao {
     @Override
     public User getUserByLogin(String login) {
         try (Connection connection = ConnectionPool.getInstance().getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(UserSqlQuery.FIND_USER_BY_LOGIN)) {
+             PreparedStatement preparedStatement = connection.prepareStatement(UserSqlQuery.FIND_USER_BY_LOGIN);) {
             preparedStatement.setString(1, login);
 
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
@@ -38,7 +39,7 @@ public class MySqlUserDao implements UserDao {
                 return new User(resultSet);
             }
         } catch (SQLException e) {
-            throw new RuntimeException("Ошибка getUserByLogin", e);
+            throw new RuntimeException(e);
         }
     }
 
@@ -52,14 +53,36 @@ public class MySqlUserDao implements UserDao {
                 users.add(new User(resultSet));
             }
         } catch (SQLException e) {
-            throw new RuntimeException("Ошибка getAllUsers", e);
+            throw new RuntimeException(e);
         }
 
         return users;
     }
 
     @Override
-    public void changeRole(long userId, int roleId) {
+    public User getUserById(Long id) {
+        try (Connection connection = ConnectionPool.getInstance().getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(UserSqlQuery.FIND_USER_BY_ID)) {
+            preparedStatement.setLong(1, id);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                // Так как id - Key, то такая строка может быть только одна
+                if (!resultSet.next()) return null;
+                return new User(resultSet);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public User logIn(String login, String password) {
+        return null;
+    }
+
+    @Override
+    public void signUp(String login, String password) {
 
     }
 
@@ -69,8 +92,35 @@ public class MySqlUserDao implements UserDao {
     }
 
     @Override
-    public Boolean delete(User object) {
-        return null;
+    public void changeRole(long userId, int roleId) {
+        try (Connection connection = ConnectionPool.getInstance().getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(UserSqlQuery.CHANGE_ROLE)) {
+            preparedStatement.setInt(1, roleId);
+            preparedStatement.setLong(2, userId);
+
+            if (preparedStatement.executeUpdate() == 0) {
+                throw new IllegalArgumentException("Неправильный аргумент (или аргументы) при смене роли");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void changePassword(long userId, String password) {
+        String hashPassword = HashPassword.hash(password.toCharArray());
+
+        try (Connection connection = ConnectionPool.getInstance().getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(UserSqlQuery.CHANGE_PASSWORD)) {
+            preparedStatement.setString(1, hashPassword);
+            preparedStatement.setLong(2, userId);
+
+            if (preparedStatement.executeUpdate() == 0) {
+                throw new IllegalArgumentException("Ошибка при попытке сменить пароль");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -79,7 +129,7 @@ public class MySqlUserDao implements UserDao {
     }
 
     @Override
-    public User find(Long id) {
+    public Boolean delete(User object) {
         return null;
     }
 }
