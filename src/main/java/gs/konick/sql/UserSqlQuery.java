4bb897109e8f88c5c898f1a19ef7cf5e2d7de019
konickSGS <konick.gevurah@gmail.com>
2024-09-26 01:58:20 +0300

    Добавлен sql find user by id

--- src/main/java/gs/konick/sql/UserSqlQuery.java
+++ src/main/java/gs/konick/sql/UserSqlQuery.java
@@ -4,6 +4,7 @@ public class UserSqlQuery extends SqlQuery {
     private static final String tableName = "users";
 
     public static final String FIND_USER_BY_LOGIN = "SELECT * FROM " + tableName + " WHERE login LIKE ?";
+    public static final String FIND_USER_BY_ID    = "SELECT * FROM " + tableName + " WHERE id = ?";
     public static final String GET_ALL_USERS      = "SELECT * FROM " + tableName;
     public static final String LOG_IN             = "SELECT * FROM " + tableName + " WHERE login LIKE ? AND password LIKE ?";
     public static final String CHANGE_PASSWORD    = "UPDATE " + tableName + " SET password = ? WHERE id = ?";
