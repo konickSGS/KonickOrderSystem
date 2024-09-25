05915a7208c529d7f310c6803219fc94b716d8f2
konickSGS <konick.gevurah@gmail.com>
2024-09-25 23:21:52 +0300

    Добавлен equals, hashcode

--- src/main/java/gs/konick/model/User.java
+++ src/main/java/gs/konick/model/User.java
@@ -2,14 +2,78 @@ package gs.konick.model;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Objects;
 
 public class User implements Serializable {
     private long id;
     private String login;
-    private long roleId;
+    private Role role;
     private String email;
     private String address;
     private Date createDate;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && Objects.equals(login, user.login) && role == user.role && Objects.equals(email, user.email) && Objects.equals(address, user.address) && Objects.equals(createDate, user.createDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, login, role, email, address, createDate);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", role=" + role +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                ", createDate=" + createDate +
+                '}';
+    }
+
+    public static class Builder {
+        User user = new User();
+
+        public Builder setId(long id) {
+            user.id = id;
+            return this;
+        }
+
+        public Builder setLogin(String login) {
+            user.login = login;
+            return this;
+        }
+
+        public Builder setRole(long roleId) {
+            user.role = Role.getRoleById(roleId);
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            user.email = email;
+            return this;
+        }
+
+        public Builder setAddress(String address) {
+            user.address = address;
+            return this;
+        }
+
+        public Builder setCreateDate(Date createDate) {
+            user.createDate = createDate;
+            return this;
+        }
+
+        public User build() {
+            return user;
+        }
+    }
 
     public long getId() {
         return id;
@@ -27,12 +91,12 @@ public class User implements Serializable {
         this.login = login;
     }
 
-    public long getRoleId() {
-        return roleId;
+    public Role getRole() {
+        return role;
     }
 
-    public void setRoleId(long roleId) {
-        this.roleId = roleId;
+    public void setRole(Role role) {
+        this.role = role;
     }
 
     public String getEmail() {
