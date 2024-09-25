d64955bd8b69a77a15b9a3a0e9506d28ad8a6326
konickSGS <konick.gevurah@gmail.com>
2024-09-25 23:55:27 +0300

    Смена порядка sql

--- src/main/java/gs/konick/sql/UserSqlQuery.java
+++ src/main/java/gs/konick/sql/UserSqlQuery.java
@@ -3,9 +3,9 @@ package gs.konick.sql;
 public class UserSqlQuery extends SqlQuery {
     private static final String tableName = "users";
 
-    public static final String LOG_IN             = "SELECT * FROM " + tableName + " WHERE login LIKE ? AND password LIKE ?";
     public static final String FIND_USER_BY_LOGIN = "SELECT * FROM " + tableName + " WHERE login LIKE ?";
     public static final String GET_ALL_USERS      = "SELECT * FROM " + tableName;
+    public static final String LOG_IN             = "SELECT * FROM " + tableName + " WHERE login LIKE ? AND password LIKE ?";
     public static final String CHANGE_PASSWORD    = "UPDATE " + tableName + " SET password = ? WHERE id = ?";
     public static final String CHANGE_ROLE        = "UPDATE " + tableName + " SET role_id = ? WHERE id = ?";
     public static final String SIGN_UP            = "INSERT INTO " + tableName + " (login, password) VALUES (?, ?)";
