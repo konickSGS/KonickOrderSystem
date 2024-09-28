b7fcf50d26471c326992b20fdbadd4761bf6b2d9
konickSGS <konick.gevurah@gmail.com>
2024-09-28 12:57:59 +0300

    Добавление sql запроса, который добавляет тестовые данные

---
+++ sql/user_test/insert_into_users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (login, hashed_password, role_id, email, address) VALUE ('manager1', 'password', 2, 'manager1@admin.ru', 'somewhere2');
+INSERT INTO users (login, hashed_password, role_id, email, address) VALUE ('user1', 'password', 1, 'user1@admin.ru', 'ulitsa1');
+INSERT INTO users (login, hashed_password, role_id, email, address) VALUE ('user2', 'password', 1, 'user2@admin.ru', 'ulitsa2');
+INSERT INTO users (login, hashed_password, role_id, email, address) VALUE ('user3', 'password', 1, 'user3@admin.ru', 'ulitsa3');
