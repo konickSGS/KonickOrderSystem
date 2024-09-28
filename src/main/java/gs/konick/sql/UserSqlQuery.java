01b8aa93543087d08966ffd9d8d89b9d5a4b2156
konickSGS <konick.gevurah@gmail.com>
2024-09-28 12:59:47 +0300

    Обновление sql запросов

--- src/main/java/gs/konick/sql/UserSqlQuery.java
+++ src/main/java/gs/konick/sql/UserSqlQuery.java
@@ -6,9 +6,9 @@ public class UserSqlQuery extends SqlQuery {
     public static final String FIND_USER_BY_LOGIN = "SELECT * FROM " + tableName + " WHERE login LIKE ?";
     public static final String FIND_USER_BY_ID    = "SELECT * FROM " + tableName + " WHERE id = ?";
     public static final String GET_ALL_USERS      = "SELECT * FROM " + tableName;
-    public static final String LOG_IN             = "SELECT * FROM " + tableName + " WHERE login LIKE ? AND password LIKE ?";
-    public static final String CHANGE_PASSWORD    = "UPDATE " + tableName + " SET password = ? WHERE id = ?";
+    public static final String LOG_IN             = "SELECT * FROM " + tableName + " WHERE login LIKE ? AND hashed_password LIKE ?";
+    public static final String CHANGE_PASSWORD    = "UPDATE " + tableName + " SET hashed_password = ? WHERE id = ?";
     public static final String CHANGE_ROLE        = "UPDATE " + tableName + " SET role_id = ? WHERE id = ?";
-    public static final String SIGN_UP            = "INSERT INTO " + tableName + " (login, password) VALUES (?, ?)";
+    public static final String SIGN_UP            = "INSERT INTO " + tableName + " (login, hashed_password, role_id, email, address) VALUES (?, ?, ?, ?, ?)";
     public static final String DELETE_USER        = "DELETE FROM " + tableName + " WHERE id = ?";
 }
