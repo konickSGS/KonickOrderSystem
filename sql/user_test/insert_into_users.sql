1fda120f9c60a951fe391a5d56673f523ad533f9
konickSGS <konick.gevurah@gmail.com>
2024-09-28 20:49:00 +0300

    Правка, который заменяет на hashed_password

--- sql/user_test/insert_into_users.sql
+++ sql/user_test/insert_into_users.sql
@@ -1,4 +1,4 @@
-INSERT INTO users (login, hashed_password, role_id, email, address) VALUE ('manager1', 'password', 2, 'manager1@admin.ru', 'somewhere2');
-INSERT INTO users (login, hashed_password, role_id, email, address) VALUE ('user1', 'password', 1, 'user1@admin.ru', 'ulitsa1');
-INSERT INTO users (login, hashed_password, role_id, email, address) VALUE ('user2', 'password', 1, 'user2@admin.ru', 'ulitsa2');
-INSERT INTO users (login, hashed_password, role_id, email, address) VALUE ('user3', 'password', 1, 'user3@admin.ru', 'ulitsa3');
+INSERT INTO users (login, hashed_password, role_id, email, address) VALUE ('manager1', 'OA+XcdLfhWbOK9W47XcrC7dP1kV/uAOrLSZ8OU2Jx1A=', 2, 'manager1@admin.ru', 'somewhere2');
+INSERT INTO users (login, hashed_password, role_id, email, address) VALUE ('user1', 'CgQblGLKpKMbrDVn4Lbm/ZEAeH2yq0M9lvbReMq/zpA=', 1, 'user1@admin.ru', 'ulitsa1');
+INSERT INTO users (login, hashed_password, role_id, email, address) VALUE ('user2', 'YCXRj+SKvUUWhSjxioLiZd2Y1CGnCEqgn2GzQXA5AaM=', 1, 'user2@admin.ru', 'ulitsa2');
+INSERT INTO users (login, hashed_password, role_id, email, address) VALUE ('user3', 'WGD68CtrxiIrpaylI1YPDjZMzYtnvuSG/ov3wB1JLMs=', 1, 'user3@admin.ru', 'ulitsa3');
