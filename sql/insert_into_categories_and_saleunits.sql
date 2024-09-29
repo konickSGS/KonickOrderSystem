0a91aea6232a81ab65b5b1ab552ea2c3b983085e
konickSGS <konick.gevurah@gmail.com>
2024-09-29 14:51:00 +0300

    Создание sql запросов для создания и заполнения categories и saleunits

---
+++ sql/insert_into_categories_and_saleunits.sql
@@ -0,0 +1,26 @@
+INSERT INTO categories (name) VALUE ('Пицца');
+INSERT INTO categories (name) VALUE ('Супы');
+INSERT INTO categories (name) VALUE ('Суши');
+INSERT INTO categories (name) VALUE ('Напитки');
+INSERT INTO categories (name) VALUE ('Другое');
+
+INSERT INTO saleunits (name, category_id, price)
+VALUE ('Маргарита', 1, 49000);
+INSERT INTO saleunits (name, category_id, price)
+VALUE ('Гавайская', 1, 58000);
+INSERT INTO saleunits (name, category_id, price)
+VALUE ('4 сыра', 1, 50000);
+INSERT INTO saleunits (name, category_id, price)
+VALUE ('Том Ям', 2, 90000);
+INSERT INTO saleunits (name, category_id, price)
+VALUE ('Рамен', 1, 55000);
+INSERT INTO saleunits (name, category_id, price)
+VALUE ('Филадельфия', 3, 80000);
+INSERT INTO saleunits (name, category_id, price)
+VALUE ('Калифорния', 3, 70000);
+INSERT INTO saleunits (name, category_id, price)
+VALUE ('Черноголовка 0.5', 4, 9000);
+INSERT INTO saleunits (name, category_id, price)
+VALUE ('Вода 0.5', 4, 5000);
+INSERT INTO saleunits (name, category_id, price)
+VALUE ('Магнитик', 5, 3000);
