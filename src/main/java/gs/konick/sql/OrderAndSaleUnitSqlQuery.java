c6ebe43ac9cc263fa6d4c97474be79a5eb3d12a7
konickSGS <konick.gevurah@gmail.com>
2024-09-29 17:44:27 +0300

    Добавление order_and_saleunit

---
+++ src/main/java/gs/konick/sql/OrderAndSaleUnitSqlQuery.java
@@ -0,0 +1,8 @@
+package gs.konick.sql;
+
+public class OrderAndSaleUnitSqlQuery {
+    private static final String manyToManyTable = "order_and_saleunit";
+
+    public static final String FIND_ALL_SALEUNITS_BY_ORDERID = "SELECT saleunit_id, count FROM " + manyToManyTable + " WHERE order_id = ?;";
+    public static final String ADD_ORDER_AND_SALEUNIT = "INSERT INTO " + manyToManyTable + " (order_id, saleunit_id, count) VALUES (?, ?, ?);";
+}
