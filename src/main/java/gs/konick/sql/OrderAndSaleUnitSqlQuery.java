ee725a3c1f8450a9d889f707db4d2e10d5ecb875
konickSGS <konick.gevurah@gmail.com>
2024-09-29 19:01:34 +0300

    Правка

--- src/main/java/gs/konick/sql/OrderAndSaleUnitSqlQuery.java
+++ src/main/java/gs/konick/sql/OrderAndSaleUnitSqlQuery.java
@@ -4,5 +4,5 @@ public class OrderAndSaleUnitSqlQuery {
     private static final String manyToManyTable = "order_and_saleunit";
 
     public static final String FIND_ALL_SALEUNITS_BY_ORDERID = "SELECT saleunit_id, count FROM " + manyToManyTable + " WHERE order_id = ?;";
-    public static final String ADD_ORDER_AND_SALEUNIT = "INSERT INTO " + manyToManyTable + " (order_id, saleunit_id, count) VALUES (?, ?, ?);";
+    public static final String ADD_ORDER_AND_SALEUNIT        = "INSERT INTO " + manyToManyTable + " (order_id, saleunit_id, count) VALUES (?, ?, ?);";
 }
