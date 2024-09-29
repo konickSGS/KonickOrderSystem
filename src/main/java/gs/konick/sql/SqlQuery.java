c666749ff50754a3944846a2260a53ede04782f2
konickSGS <konick.gevurah@gmail.com>
2024-09-29 20:03:42 +0300

    Добавление rollback

--- src/main/java/gs/konick/sql/SqlQuery.java
+++ src/main/java/gs/konick/sql/SqlQuery.java
@@ -1,4 +1,29 @@
 package gs.konick.sql;
 
+import org.apache.logging.log4j.LogManager;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Savepoint;
+
 public abstract class SqlQuery {
+    /**
+     * rollback connection
+     * @param connection connection
+     */
+    public static void rollback(Connection connection) {
+        try {
+            connection.rollback();
+        } catch (SQLException e) {
+            LogManager.getLogger(SqlQuery.class.getName()).error(e);
+        }
+    }
+
+    public static void rollback(Connection connection, Savepoint savepoint) {
+        try {
+            connection.rollback(savepoint);
+        } catch (SQLException e) {
+            LogManager.getLogger(SqlQuery.class.getName()).error(e);
+        }
+    }
 }
