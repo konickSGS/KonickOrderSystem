8ab5478a3cb3940a64b38602d58df9cd87095d0d
konickSGS <konick.gevurah@gmail.com>
2024-09-29 20:04:21 +0300

    Удаление лишних строк

--- src/main/java/gs/konick/sql/OrderSqlQuery.java
+++ src/main/java/gs/konick/sql/OrderSqlQuery.java
@@ -2,9 +2,8 @@ package gs.konick.sql;
 
 public class OrderSqlQuery extends SqlQuery {
     private static final String tableName = "orders";
-    private static final String manyToManyTable1 = "order_and_saleunit";
-    private static final String manyToManyTable2 = "basket_and_saleunit";
 
+    public static final String FIND_ORDER_BY_ID              = "SELECT * FROM " + tableName + " WHERE id = ?";
     public static final String FIND_ALL_ORDERS_BY_USER_ID    = "SELECT * FROM " + tableName + " WHERE user_id = ?;";
     public static final String GET_ALL_ORDERS                = "SELECT * FROM " + tableName;
     public static final String GET_FILTERED_ORDERS_BY_STATUS = "SELECT * FROM " + tableName + " WHERE status_id = ?;";
